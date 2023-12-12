da67cf54089d4078d5cab7b64a535ab499007099
Libby Lebyane <lebyane.lm@gmail.com>
2023-12-12 19:05:24 +0200

    Added more fields to returned currency conversion request

--- server.py
+++ server.py
@@ -47,6 +47,7 @@ def convert_a_currency(_from_amount: str, _from_currency: str, _to_currency: str
         latest_conversions = currency_client.latest(_from_currency)
 
         if latest_conversions["data"].get(_from_currency):
+            currency_details = list_all_currencies().json["data"].get(_to_currency)
             return Response(
                 cd=200,
                 dt=dict(
@@ -55,6 +56,9 @@ def convert_a_currency(_from_amount: str, _from_currency: str, _to_currency: str
                     exchange_rate=latest_conversions["data"][_to_currency],
                     from_amount=_from_amount,
                     to_amount=latest_conversions["data"][_to_currency] * from_amount,
+                    name=currency_details.get("name"),
+                    name_plural=currency_details.get("name_plural"),
+                    symbol=currency_details.get("symbol")
                 ),
             ).to_json()
         else:
@@ -82,134 +86,134 @@ def list_all_currencies():
         "ZAR": {
             "code": "ZAR",
             "name": "South African Rand",
-            "name_plural": "South African rand",
+            "name_plural": "Rands",
             "symbol": "R",
         },
         "EUR": {"code": "EUR", "name": "Euro", "name_plural": "Euros", "symbol": "€"},
         "USD": {
             "code": "USD",
             "name": "US Dollar",
-            "name_plural": "US dollars",
+            "name_plural": "U$ Dollars",
             "symbol": "$",
         },
         "JPY": {
             "code": "JPY",
             "name": "Japanese Yen",
-            "name_plural": "Japanese yen",
+            "name_plural": "Yens",
             "symbol": "￥",
         },
         "BGN": {
             "code": "BGN",
             "name": "Bulgarian Lev",
-            "name_plural": "Bulgarian leva",
+            "name_plural": "Levi",
             "symbol": "лв.",
         },
         "CZK": {
             "code": "CZK",
             "name": "Czech Republic Koruna",
-            "name_plural": "Czech Republic korunas",
+            "name_plural": "Korunas",
             "symbol": "Kč",
         },
         "DKK": {
             "code": "DKK",
             "name": "Danish Krone",
-            "name_plural": "Danish kroner",
+            "name_plural": "Kroner",
             "symbol": "kr",
         },
         "GBP": {
             "code": "GBP",
             "name": "British Pound Sterling",
-            "name_plural": "British pounds sterling",
+            "name_plural": "Sterling",
             "symbol": "£",
         },
         "HUF": {
             "code": "HUF",
             "name": "Hungarian Forint",
-            "name_plural": "Hungarian forints",
+            "name_plural": "Forints",
             "symbol": "Ft",
         },
         "PLN": {
             "code": "PLN",
             "name": "Polish Zloty",
-            "name_plural": "Polish zlotys",
+            "name_plural": "Zlotys",
             "symbol": "zł",
         },
         "RON": {
             "code": "RON",
             "name": "Romanian Leu",
-            "name_plural": "Romanian lei",
+            "name_plural": "Lei",
             "symbol": "RON",
         },
         "SEK": {
             "code": "SEK",
             "name": "Swedish Krona",
-            "name_plural": "Swedish kronor",
+            "name_plural": "Kronor",
             "symbol": "kr",
         },
         "CHF": {
             "code": "CHF",
             "name": "Swiss Franc",
-            "name_plural": "Swiss francs",
+            "name_plural": "Francs",
             "symbol": "CHF",
         },
         "ISK": {
             "code": "ISK",
             "name": "Icelandic Króna",
-            "name_plural": "Icelandic krónur",
+            "name_plural": "Krónur",
             "symbol": "kr",
         },
         "NOK": {
             "code": "NOK",
             "name": "Norwegian Krone",
-            "name_plural": "Norwegian kroner",
+            "name_plural": "Kroner",
             "symbol": "kr",
         },
         "HRK": {
             "code": "HRK",
             "name": "Croatian Kuna",
-            "name_plural": "Croatian kunas",
+            "name_plural": "Kunas",
             "symbol": "kn",
         },
         "RUB": {
             "code": "RUB",
             "name": "Russian Ruble",
-            "name_plural": "Russian rubles",
+            "name_plural": "Rubles",
             "symbol": "py6.",
         },
         "TRY": {
             "code": "TRY",
             "name": "Turkish Lira",
-            "name_plural": "Turkish Lira",
+            "name_plural": "Lira",
             "symbol": "TL",
         },
         "AUD": {
             "code": "AUD",
             "name": "Australian Dollar",
-            "name_plural": "Australian dollars",
-            "symbol": "$",
+            "name_plural": "A$ Dollars",
+            "symbol": "A$",
         },
         "BRL": {
             "code": "BRL",
             "name": "Brazilian Real",
-            "name_plural": "Brazilian reals",
+            "name_plural": "Reals",
             "symbol": "R$",
         },
         "CAD": {
             "code": "CAD",
             "name": "Canadian Dollar",
-            "name_plural": "Canadian dollars",
-            "symbol": "$",
+            "name_plural": "C$ Dollars",
+            "symbol": "C$",
         },
         "CNY": {
             "code": "CNY",
             "name": "Chinese Yuan",
-            "name_plural": "Chinese yuan",
+            "name_plural": "Yuan",
             "symbol": "CN¥",
         },
         "HKD": {
             "code": "HKD",
             "name": "Hong Kong Dollar",
-            "name_plural": "Hong Kong dollars",
+            "name_plural": "H$ Dollars",
             "symbol": "$",
         },
         "IDR": {
@@ -221,25 +225,25 @@ def list_all_currencies():
         "ILS": {
             "code": "ILS",
             "name": "Israeli New Sheqel",
-            "name_plural": "Israeli new sheqels",
+            "name_plural": "Sheqels",
             "symbol": "₪",
         },
         "INR": {
             "code": "INR",
             "name": "Indian Rupee",
-            "name_plural": "Indian rupees",
+            "name_plural": "Rupees",
             "symbol": "টকা",
         },
         "KRW": {
             "code": "KRW",
             "name": "South Korean Won",
-            "name_plural": "South Korean won",
+            "name_plural": "Won",
             "symbol": "₩",
         },
         "MXN": {
             "code": "MXN",
             "name": "Mexican Peso",
-            "name_plural": "Mexican pesos",
+            "name_plural": "Pesos",
             "symbol": "$",
         },
         "MYR": {
@@ -257,19 +261,19 @@ def list_all_currencies():
         "PHP": {
             "code": "PHP",
             "name": "Philippine Peso",
-            "name_plural": "Philippine pesos",
+            "name_plural": "Pesos",
             "symbol": "₱",
         },
         "SGD": {
             "code": "SGD",
             "name": "Singapore Dollar",
-            "name_plural": "Singapore dollars",
+            "name_plural": "S$ Dollars",
             "symbol": "$",
         },
         "THB": {
             "code": "THB",
             "name": "Thai Baht",
-            "name_plural": "Thai baht",
+            "name_plural": "Baht",
             "symbol": "฿",
         },
     }
